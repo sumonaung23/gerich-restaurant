0cc1f4770228dd23f8f9ec55a60928887515cbbe
sumonaung23 <sumonaung575@gmail.com>
2023-10-12 21:25:07 +0630

    structured navbar with logo, links and login / table

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import { GiHamBurgerMenu } from 'react-icons/gi';
+import { MdOutlineResturantMenu }  from 'react-icons/md';
+
+import images from '../../constants/images';
 
 import './Navbar.css';
 
 const Navbar = () => (
-  <div>
-    Navbar
-  </div>
+  <nav className='app__navbar'>
+    <div className='app__navbar-logo'>
+      <img src={images.gericht} alt='app logo' />
+    </div>
+    <ul className='app__navbar-links'>
+      <li className='p__opensans'><a href="#home">Home</a></li>
+      <li className='p__opensans'><a href="#about">About</a></li>
+      <li className='p__opensans'><a href="#menu">Menu</a></li>
+      <li className='p__opensans'><a href="#awards">Awards</a></li>
+      <li className='p__opensans'><a href="#contact">Contact</a></li>
+    </ul>
+    <div className='app__navbar-login'>
+        <a href="#login" className='p__opensans'>Login / Register</a>
+        <div />
+        <a href="/" className='p__opensans'>Book Table</a>
+    </div>
+  </nav>
 );
 
 export default Navbar;
