829b880842c0ecea9253cd62847ae072cfa088f6
sumonaung23 <sumonaung575@gmail.com>
2023-10-12 23:36:20 +0630

    structured header with two sections, info and img

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
-
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app__wrapper section__padding' id='home'>
+    <div className='app__wrapper_info'>
+      <SubHeading title="Chase the new flavor" />
+      <h1 className='app__header-h1'>The key to Fine Dining</h1>
+      <p className='p__opensans' style={{margin: '2rem 0'}}>
+        Sit Tellus Sed Senectus Vivamus Molestie. Condimentum Volutpat Morbi Facillisis Quam Sceierisqe Sapien. Penatbus Aliquam Amet Tellus
+      </p>
+      <button type='button' className='custom__button'>Explore Menu</button>
+    </div>
+    <div className='app__wrapper_img'>
+      <img src={images.welcome} alt='header img' />
+    </div>
   </div>
 );
 
