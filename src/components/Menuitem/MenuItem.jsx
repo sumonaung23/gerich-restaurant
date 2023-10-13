c1a6c2fd2ab74b2bd10d64d56c0d8fd2b29801c9
sumonaung23 <sumonaung575@gmail.com>
2023-10-13 13:59:51 +0630

    linked menuItem to the Menu component including props value

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -2,9 +2,23 @@ import React from 'react';
 
 import './MenuItem.css';
 
-const MenuItem = () => (
-  <div>
-    MenuItem
+const MenuItem = ({key, title,price, tags}) => (
+  <div className='app__menuItem'>
+    <div className="app__menuItem-head">
+      <div className="app__menuItem-name">
+        <p className="p__cormorant" style={{color: '#DCCA87'}}>{title} </p>
+      </div>
+
+      <div className="app__menuItem-dash" />
+
+      <div className="app__menuItem-price">
+        <p className="p__cormorant">{price} </p>
+      </div>
+    </div>
+
+    <div className="app__menuItem-sub">
+      <p className="p__opensans" style={{color: '#AAA'}}>{tags}</p>
+    </div>
   </div>
 );
 
